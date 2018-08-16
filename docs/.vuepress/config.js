8836ad40c69e33d371be8d7472a77cd0b345047a
suedoe <suedoe8080@gmail.com>
2018-08-16 11:10:41 +0900

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: 'Javascript Study',
+  description: 'Mopsy & Valista',
+  themeConfig: {
+    sidebar: [
+      '/',
+      ['/study/1', 'Nice syntax in JS'],
+    ]
+  },
+}
